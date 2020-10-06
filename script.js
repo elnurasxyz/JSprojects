2d7d2216200d057410530f7f4218487084c0caea
Elnuras <Elnuras@gmail.com>
2020-10-07 01:17:30 +0700

    Created Animal years

---
+++ script.js
@@ -0,0 +1,10 @@
+let myAge = prompt("Enter your age"); //Set my age
+let earlyYears = 2; // Set the early years
+earlyYears *=10.5;
+laterYears = myAge;// To assign  late years
+laterYears*=4;//calculate the LaterYears
+console.log(`${laterYears} , ${earlyYears}`)
+myAgeInDogYears = laterYears + earlyYears;//Calculate the dog's year
+let myName = prompt("Enter your Name");
+myName = myName.toLowerCase();//Convert it to lover case
+alert(`My name is  ${myName} I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years`)
